fdb6fc7df8f6c4c201e0ed0f4a130d8bbedba68b
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-04 14:07:37 -0500

    working broadcast

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import type { ServerWebSocket } from "bun";
 import { Hono } from "hono";
 import { createBunWebSocket } from "hono/bun";
+import { WSContext } from "hono/ws";
 
 const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
 
 const app = new Hono();
+const clients = new Set<WSContext<ServerWebSocket>>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -16,13 +18,20 @@ app.get(
     return {
       onMessage(event, ws) {
         console.log(`Message from client: ${event.data}`);
-        ws.send(event.data.toString()); // send the same message back to the client
+        // Broadcast the message to all connected clients
+        const message = event.data.toString();
+        console.log(`Broadcasting message to ${clients.size} clients`);
+        clients.forEach((client) => {
+          client.send(message);
+        });
       },
-      onClose: () => {
+      onClose: (event, ws) => {
         console.log("Connection closed");
+        clients.delete(ws);
       },
-      onOpen: () => {
-        console.log(`Connection opened from`);
+      onOpen: (event, ws) => {
+        console.log(`Connection opened`);
+        clients.add(ws);
       },
       onError: (error) => {
         console.error("Error", error);
