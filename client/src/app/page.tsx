b56c844536c940f5313ae75152d5b3c442df5d6a
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-03-06 14:58:46 -0500

    fix build

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 import { Join } from "@/components/Join";
-import { Syncer } from "@/components/Syncer";
-import { useState } from "react";
 
 export default function Home() {
-  const [showSyncer, setShowSyncer] = useState(false);
-
-  if (!showSyncer) {
-    return <Join />;
-  }
-
-  return <Syncer />;
+  return <Join />;
 }
