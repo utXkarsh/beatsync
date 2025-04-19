f5bb5c2a8197405c2dc76c5a2ddf143255213455
Freeman Jiang <freeman.jiang.ca@gmail.com>
2025-04-18 22:57:46 -0400

    remove gain on bottom

--- apps/client/src/components/dashboard/Bottom.tsx
+++ apps/client/src/components/dashboard/Bottom.tsx
@@ -1,15 +1,11 @@
 import { motion } from "motion/react";
 import { Player } from "../room/Player";
-import { GainMeter } from "./GainMeter";
 
 export const Bottom = () => {
   return (
     <motion.div className="flex-shrink-0 border-t border-neutral-800/50 bg-neutral-900/10 backdrop-blur-lg p-4 shadow-[0_-5px_15px_rgba(0,0,0,0.1)] z-10 relative">
       <div className="max-w-3xl mx-auto">
         <Player />
-      </div>
-      <div className="absolute bottom-4 right-4 hidden lg:flex">
-        <GainMeter />
       </div>
     </motion.div>
   );
